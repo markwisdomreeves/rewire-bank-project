f8deff227f5e94b9972dbd243c0936285f172bc9
Mark Wisdom Reeves <46942868+markwisdomreeves@users.noreply.github.com>
2021-05-29 17:41:29 +0200

    Update config.js

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,7 +1,7 @@
 
 const REACT_BACKEND_API = "https://bank-c.herokuapp.com";
-const REACT_APP_GOOGLE_CLIENT_ID = "310699671680-jbpk4an2ad8qpdbhcu2e4l92r0vl3nee.apps.googleusercontent.com";
-const REACT_APP_FACEBOOK_APP_ID = "160233942718913";
+const REACT_APP_GOOGLE_CLIENT_ID = "";
+const REACT_APP_FACEBOOK_APP_ID = "";
 
 export {
   REACT_BACKEND_API,
